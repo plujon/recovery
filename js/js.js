52ef1449d398d503c0002e68716d980136e852d5
Jon Wilkes <jon@pluckeye.net>
2017-06-05 09:23:53 -0700

    Add bells and whistles (shuffle, export, visibility).
    I'm impressed by how easy this is; hooray for DataTables!

--- js/js.js
+++ js/js.js
@@ -1,3 +1,50 @@
+function shuffle() {
+  var trs = document.querySelectorAll('tr'),
+      i = trs.length,
+      tr;
+  for (; i > 1; i -= 1) {
+    tr = trs.item(Math.floor(Math.random() * i));
+    tr.parentNode.appendChild(tr);
+  }
+}
+
+function editThisPage() {
+  window.location = "http://github.com/plujon/recovery";
+}
+
 $(function () {
-  $('#thetable').dataTable();
+  var table = $('#thetable').DataTable({
+    dom: 'Bfrtip',
+    colReorder: true,
+    buttons: [
+      {
+        action: shuffle,
+        text: '<span class="glyphicon glyphicon-refresh"></span> shuffle</div>'
+      },
+      {
+        extend: 'copy',
+        titleAttr: 'Copy visible data to clipboard.',
+        text: '<span class="glyphicon glyphicon-copy"></span> copy</div>'
+      },
+      {
+        extend: 'csv',
+        titleAttr: 'Export data as csv.',
+        text: '<span class="glyphicon glyphicon-download-alt"></span> csv</div>'
+      },
+      {
+        extend: 'pdf',
+        titleAttr: 'Export data as pdf.',
+        text: '<span class="glyphicon glyphicon-download-alt"></span> pdf</div>'
+      },
+      {
+        extend: 'colvis',
+        titleAttr: 'Select visible columns.',
+        text: '<span class="glyphicon glyphicon-eye-close"></span> columns</div>'
+      },
+      {
+        action: editThisPage,
+        titleAttr: 'If you do not know how to use github, feel free to use the contact me link on the github page.  If you would like to see a built-in table editor, that could be done.',
+        text: '<span class="glyphicon glyphicon-edit"></span> edit</div>'
+      }
+    ]});
 });
